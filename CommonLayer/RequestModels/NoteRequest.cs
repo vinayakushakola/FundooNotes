a4e4627c6bdb39257ce713826c3d11e33595cfcd
vinayakushakola <vinayak.ushakola@gmail.com>
2020-05-05 12:43:15 +0530

    Added NoteRequest & NoteResponse Model

---
+++ CommonLayer/RequestModels/NoteRequest.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CommonLayer.RequestModels
+{
+    class NotesRequest
+    {
+    }
+}
---
+++ CommonLayer/ResponseModels/UserNotesResponseData.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CommonLayer.ResponseModels
+{
+    class UserNotesResponseData
+    {
+    }
+}
